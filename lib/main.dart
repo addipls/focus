8d64f50440a5220bc5a69259392ab33bf43a1eca
addi <72429964+addipls@users.noreply.github.com>
2022-11-01 01:38:51 -0400

    Create base project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:focus/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FOCUS!',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const LoginScreen(),
+    );    
+  }
+}
+
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: width,
+                height: height * .5,
+                child: Image.asset("assets/focuslogo.png"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
